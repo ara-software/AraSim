684c5b22b069fe9c0a0b3a221c8ba7ed0ccd4936
Carl Pfendner <cgp@alumni.upenn.edu>
2017-07-27 13:34:25 +0000

    Fixed a typo in Settings.cc
    git-svn-id: https://delos.mps.ohio-state.edu/RadioSim/AraSim/branches/new_geom@729 dd60067b-e629-4079-a9b7-6a9fd983260e

--- Settings.cc
+++ Settings.cc
@@ -780,7 +780,7 @@ int Settings::CheckCompatibilities(Detector *detector) {
         num_err++;
     }
 
-    if (DATA_LIKE_OUTPUT != 0 && (DETECTOR_STATION>3) {
+    if (DATA_LIKE_OUTPUT != 0 && (DETECTOR_STATION>3)) {
         cerr<<"DATA_LIKE_OUTPUT=1,2 doesn't work with DETECTOR_STATION>3"<<endl;
         cerr<<"DATA_LIKE_OUTPUT controls data-like output into UsefulAtriStationEvent format; without a real station selected (using DETECTOR==3,4), the mapping to the data-like output will not function correctly"<<endl;
         num_err++;
