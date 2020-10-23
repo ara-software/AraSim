cf3ef5c3c3813fb545b86792a4ff2c79a579e4a0
Brian Clark <baclark@msu.edu>
2020-10-22 22:12:17 -0400

    Add freq dep att length (#24)
    * add the relevant icemodels
    
    * actually use the freq dependent attenuation length in the Report class
    
    * update docs in Settings.h
    
    * do attenuation length calculation at the ray midpoint
    
    * fix some typos

--- Settings.h
+++ Settings.h
@@ -43,7 +43,7 @@ class Settings
         int CONSTANTICETHICKNESS; // set ice thickness to constant value
         int FIXEDELEVATION; // fix the elevation to the thickness of ice.
         int MOOREBAY; //1=use Moore's Bay measured ice field attenuation length for the west land, otherwise use South Pole data
-        int USE_ARA_ICEATTENU; // 0 : use old ice attenuation factor with one depth info, 1 : (default) use ARA measured ice attenuation factor with depth from ray steps
+        int USE_ARA_ICEATTENU; // 0 : use old ice attenuation factor with one depth info, 1 : (default) use ARA measured ice attenuation factor with depth from ray steps, 2: use frequency dependent model with depth from ray steps
 
 	int Z_THIS_TOLERANCE; // 0 : (default) use default 'requiredAccuracy' parameter for ray tracing, 1 : change 'requiredAccuracy' parameter by Z_TOLERANCE
 	
