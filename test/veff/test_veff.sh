6bee7096f3f1b5034c3fde58763427d8a76a8936
Brian Clark <baclark@msu.edu>
2021-03-13 23:24:52 -0600

    does AraSim even run

--- test/veff/test_veff.sh
+++ test/veff/test_veff.sh
@@ -1,9 +1,11 @@
 # run AraSim with the veff setup file
 
-./AraSim test/veff/setup_veff_test.txt 0 outputs/. &> test/veff/veff_test_output.txt &
+./AraSim test/veff/setup_veff_test.txt 0 outputs/. 
 
-wait 
+# &> test/veff/veff_test_output.txt &
 
-# then, do a comparison to check for consistency
+# wait 
 
-python3 test/veff/check_veff.py test/veff/veff_test_output.txt
+# # then, do a comparison to check for consistency
+
+# python3 test/veff/check_veff.py test/veff/veff_test_output.txt
