f4d6593f66a6ba0bb2d099a1873dcccf019a5ff4
Justin Flaherty <jflaherty13@owens-rw01.ten.osc.edu>
2024-02-23 13:15:04 -0500

    Updating the Veff test to reflect the new effective height calculation.  Had to change the seed in order to get a non-zero total weight of passing events.

--- test/veff/check_veff.py
+++ test/veff/check_veff.py
@@ -4,8 +4,8 @@ import sys
 	Expected number of global events passing
 	And summed weight passing
 '''
-expected_global_pass = 3
-expected_total_weight = 1.99067
+expected_global_pass = 2 #Updated this from 3 after changing the effective height calculation and number of events thrown - JCF 2/23/20242
+expected_total_weight = 0.999199  #1.99067
 expected_total_weight_sigma = 0.0001
 
 
