1a181f18463e8392401190fbb566d0ebe04666a9
Justin Flaherty <jflaherty13@owens-rw01.ten.osc.edu>
2024-03-13 12:04:55 -0400

    Updating veff test to reflect new effective height calculation.  Using the same seed as before, we get one more event passing for a total of four with a higher total weight.

--- test/veff/check_veff.py
+++ test/veff/check_veff.py
@@ -4,8 +4,8 @@ import sys
 	Expected number of global events passing
 	And summed weight passing
 '''
-expected_global_pass = 2
-expected_total_weight = 0.998305
+expected_global_pass = 4
+expected_total_weight = 2.94955
 expected_total_weight_sigma = 0.0001
 
 
