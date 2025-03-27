c0a0c7b53919c9a9b831de41c8a305eb9571052a
Abigail Bishop <ms.abigailbishop@gmail.com>
2025-03-27 13:40:06 -0500

    Convert this warning comment block to a GitHub issue

--- Report.cc
+++ Report.cc
@@ -5137,26 +5137,7 @@ void Report::checkPATrigger(
         if(settings1->TRIG_ANALYSIS_MODE == 2) { // Noise only triggers
             avgSnr = pa_force_trigger_snr; 
         }
-        // // ARB 7/7/23: 
-        // //    I don't trust the signal+noise trigger estimator right now 
-        // //    so it's commented out below. This is because the trigger 
-        // //    is determined based on PA trigger efficiency that was
-        // //    calculated with signal-only triggers IIRC. This means
-        // //    using signal+noise will artificially inflate the trigger rate.
-        // //    I think we'll need to develop trigger efficiency as a function
-        // //    of signal+noise data or actually build the actual physical
-        // //    triggering pipeline (forming beams, etc) before we can 
-        // //    run PA simulations over signal+noise. 
-        // //    This is a project for the future. 
-        // else if (settings1->TRIG_ANALYSIS_MODE==1) // Noise + signal triggers
-        //     // Estimate average SNR in topmost vpol
-        //     if(stations[i].strings[0].antennas[8].V.size()>raySolNum) {
-        //         avgSnr = getAverageSNR(stations[i].strings[0].antennas[8].V_noise);
-        //     }
-        //     else {
-        //         avgSnr = 0.0;
-        //     }
-        else { // signal only triggers
+        else { 
             // Estimate average SNR in topmost vpol
             if(stations[i].strings[0].antennas[8].V.size()>raySolNum) {
                 //   it as the noise WF to get_SNR() (since the RMS of an 
@@ -5166,7 +5147,9 @@ void Report::checkPATrigger(
                 double ant_noise_voltage_RMS = trigger->GetAntNoise_voltageRMS(trigger_ch_ID, settings1);
                 tmp_noise_RMS.push_back( ant_noise_voltage_RMS );
 
-                // Calculate SNR in this antenna
+                // Calculate SNR in this antenna using trace w/o noise since 
+                //   this SNR is used with a signal-only SNR efficiency curve 
+                //   to estimate trigger likelihood
                 avgSnr = get_SNR( 
                     stations[i].strings[0].antennas[8].V_convolved, 
                     tmp_noise_RMS);
