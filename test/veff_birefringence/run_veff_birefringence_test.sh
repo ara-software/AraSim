e81b6d22e0097ef39f426a01d22fa79c91f8d36b
Alan Salcedo Gomez <alansalgo@cobalt-14.icecube.wisc.edu>
2025-04-15 13:32:03 -0500

    Changes to Veff test. Modifying max_total_bin defines a new noise array and thus changes calls to RNG

--- test/veff_birefringence/run_veff_birefringence_test.sh
+++ test/veff_birefringence/run_veff_birefringence_test.sh
@@ -4,7 +4,7 @@
 
 # then, do a comparison to check for consistency
 
-EXPECTED_GLOBAL_PASS=3
-EXPECTED_TOTAL_WEIGHT=0.0584521
+EXPECTED_GLOBAL_PASS=4
+EXPECTED_TOTAL_WEIGHT=2.89898
 EXPECTED_TOTAL_WEIGHT_SIGMA=0.0001
 python3 test/check_sim.py test/veff_birefringence/veff_birefringence_test_output.txt $EXPECTED_GLOBAL_PASS $EXPECTED_TOTAL_WEIGHT $EXPECTED_TOTAL_WEIGHT_SIGMA
