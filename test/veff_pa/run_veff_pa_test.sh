9b77fc9df174040e8ee3a3b0e90f318a9abd6941
Marco Muzio <msm6428@psu.edu>
2025-05-10 00:21:21 -0500

    Update PA veff test.

--- test/veff_pa/run_veff_pa_test.sh
+++ test/veff_pa/run_veff_pa_test.sh
@@ -5,6 +5,6 @@
 # then, do a comparison to check for consistency
 
 EXPECTED_GLOBAL_PASS=8
-EXPECTED_TOTAL_WEIGHT=2.98386
+EXPECTED_TOTAL_WEIGHT=1.9697
 EXPECTED_TOTAL_WEIGHT_SIGMA=0.0001
 python3 test/check_sim.py test/veff_pa/veff_pa_test_output.txt $EXPECTED_GLOBAL_PASS $EXPECTED_TOTAL_WEIGHT $EXPECTED_TOTAL_WEIGHT_SIGMA
